376ffc0b0e52d775ad026cea63b7cb9ef1c10aa9
Jack Pickard <jack@hertilityhealth.com>
2025-05-16 16:00:51 +0100

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
 import Link from "next/link";
 import { HydrateClient } from "~/trpc/server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { createServerClient } from "@supabase/ssr";
 
 export default async function Home() {
+  const cookieStore = await cookies();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/olympics");
+  }
+
   return (
     <HydrateClient>
       <main className="bg-greek-gradient flex min-h-screen flex-col items-center justify-center text-white">
